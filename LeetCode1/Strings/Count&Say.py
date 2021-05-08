7af7c13593157c837bfc5158bc457e2c2127de93
Austin <Austin_dun@outlook.com>
2021-05-08 10:41:50 -0600

    Haystack

---
+++ LeetCode1/Strings/Count&Say.py
@@ -0,0 +1,8 @@
+number = 1
+times = 3
+
+def countsay(start, n):
+    
+
+test1 = countsay(number, times)
+print(test1)
--- LeetCode1/Strings/Haystack.py
+++ LeetCode1/Strings/Haystack.py
@@ -1,11 +1,13 @@
-haystack = "hello"
-needle = "ll"
+haystack = "helo"
+needle = ""
 
 def haystackindex(stack, needle):
-    new = []
-    for i in stack:
-        new.append(i)
-    return new
+    if len(needle) == 0:
+        return 0
+    for i in range(len(stack)):
+        if stack[i:i+len(needle)] == needle:
+            return i
+    return -1
 
 user1 = haystackindex(haystack,needle)
 print(user1)
