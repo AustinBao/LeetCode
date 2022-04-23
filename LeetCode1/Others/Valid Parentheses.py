8735116bc8fb88c33e09ef56d0459b51fed6a959
Austin Bao <Austin_dun@outlook.com>
2022-04-23 16:52:18 -0600

    Parentheses

--- LeetCode1/Others/Valid Parentheses.py
+++ LeetCode1/Others/Valid Parentheses.py
@@ -2,30 +2,17 @@ def parenthesesValid(str_of_parentheses):
     list_of_parentheses = []
     for items in str_of_parentheses:
         list_of_parentheses.append(items)
-
-    op_brackets = list_of_parentheses[0::2]
-    cl_brackets = list_of_parentheses[1::2]
-    if len(op_brackets) != len(cl_brackets):
+    if len(list_of_parentheses) % 2 != 0:
         return False
     else:
-        index = 0
-        while index != len(op_brackets):
-            if op_brackets[index] == "(":
-                if cl_brackets[index] != ")":
-                    return False
-            if op_brackets[index] == "[":
-                if cl_brackets[index] != "]":
-                    return False
-            if op_brackets[index] == "{":
-                if cl_brackets[index] != "}":
-                    return False
-            index += 1
-    return True
-
+        quick_list = [list_of_parentheses[0]]
+        for index,value in enumerate(str_of_parentheses):
+            if quick_list[index] != value:
+                quick_list.append(value)
 
 print(parenthesesValid("("))
 print(parenthesesValid("(}"))
 print(parenthesesValid("({)}"))
 print(parenthesesValid("(){}"))
-print(parenthesesValid("(){}[][][](){}{)"))
-
+print(parenthesesValid("({})"))
+print(parenthesesValid("{{}[][[[]]]}"))
--- LeetCode1/Test.py
+++ LeetCode1/Test.py
@@ -1,7 +1,5 @@
-our_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-chunked_list = list()
-chunk_size = 3
-for i in range(0, len(our_list), chunk_size):
-    chunked_list.append(our_list[i:i+chunk_size])
-print(chunked_list)
-# Returns: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11]]
+for x,y in enumerate(["a","b","c","d"]):
+    print(x,y)
+
+list = []
+print(list[0])
