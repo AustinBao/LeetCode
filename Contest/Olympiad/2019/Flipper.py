811bfbb563e937076394114274f6af75ef11de4b
Austin Bao <Austin_dun@outlook.com>
2022-07-06 23:19:45 -0600

    flipper

---
+++ Contest/Olympiad/2019/Flipper.py
@@ -0,0 +1,17 @@
+def flipper(instructions):
+    grid = [[1,2],
+            [3,4]]
+
+    for letter in instructions:
+        if letter == "H":
+            grid.reverse()
+        if letter == "V":
+            grid[0].reverse()
+            grid[1].reverse()
+
+    return grid
+
+print(flipper("VHHHV"))
+
+
+
